ae962e68c384e4c7bb2bd13612e8068a61f66ff5
Javier Salcedo <javiersalcedo.p@gmail.com>
2023-06-20 22:23:31 +0100

    Start implementing the Scanner type (chapter 4.4)

--- Sources/Scanner.swift
+++ Sources/Scanner.swift
@@ -2,14 +2,49 @@ struct Scanner
 {
     init(source: String)
     {
-        // TODO:
+        self.source = source
     }
 
 
 
-    func scan_tokens() -> [Token]
+    // MARK: - Public methods
+    mutating func scan_tokens() -> [Token]
     {
-        // TODO:
-        return []
+        while (!self.is_at_end())
+        {
+            self.current_lexeme_start = self.current_character
+            self.scan_token()
+        }
+
+        self.tokens.append(
+            Token(type:     .EOF,
+                  lexeme:   "",
+                  literal:  nil,
+                  line:     self.current_line))
+
+        return self.tokens
     }
+
+
+
+    // MARK: - Private methods
+    private func is_at_end() -> Bool { current_character >= source.count }
+
+
+
+    private func scan_token()
+    {
+        // TODO: self.advance()
+    }
+
+
+
+    // MARK: - Private members
+    private let source: String
+    private var tokens: [Token] = []
+
+    // Indices
+    private var current_lexeme_start:   Int = 0
+    private var current_character:      Int = 0
+    private var current_line:           Int = 1
 }
