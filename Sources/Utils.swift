396a581da1b246778ed7b2871ad223b1d66bc254
Javier Salcedo <javiersalcedo.p@gmail.com>
2023-09-04 21:36:39 +0100

    Add `is_letter` and `is_alphanumeric` utility functions

--- Sources/Utils.swift
+++ Sources/Utils.swift
@@ -1,2 +1,6 @@
 // I don't want to use the `isNumber` property because I only want basic single digits
 func is_digit(_ c: Character) -> Bool { c >= "0" && c <= "9" }
+
+func is_letter(_ c: Character) -> Bool { (c >= "a" && c <= "z") || (c >= "A" && c <= "Z") }
+
+func is_alphanumeric(_ c: Character) -> Bool { is_letter(c) || is_digit(c) }
