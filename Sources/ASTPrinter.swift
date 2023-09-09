2184a02690c77b5049552983f7e1b3e7e082d7fe
Javier Salcedo <javiersalcedo.p@gmail.com>
2023-09-09 13:14:54 +0100

    Add a Reverse Polish Notation (RPN) printer (Ex. 5.3)

--- Sources/ASTPrinter.swift
+++ Sources/ASTPrinter.swift
@@ -69,3 +69,76 @@ struct ASTPrinter : Visitor
         return output
     }
 }
+
+
+
+struct ASTPrinterReversePolishNotation: Visitor
+{
+    typealias R = String
+
+    public func print(expression: any Expression) -> String { expression.accept(visitor: self) }
+
+    /// "Prettyfies" a `Binary` expression
+    /// - Parameter binary: The expression to transform
+    /// - Returns: The "prettyfied" expression
+    public func visit(_ binary: Binary) -> String
+    {
+        convert_to_RPN(
+            name: binary.op.lexeme,
+            expressions:
+                binary.left,
+                binary.right)
+    }
+
+
+
+    /// "Prettyfies" a `Grouping` expression
+    /// - Parameter grouping: The expression to transform
+    /// - Returns: The "prettyfied" expression
+    public func visit(_ grouping: Grouping) -> String
+    {
+        convert_to_RPN(name: "group", expressions: grouping.expression)
+    }
+
+
+
+    /// "Prettyfies" a `LiteralExp` expression
+    /// - Parameter literalexp: The expression to transform
+    /// - Returns: The "prettyfied" expression
+    public func visit(_ literalexp: LiteralExp) -> String
+    {
+        guard let value = literalexp.value else
+        {
+            return "nil"
+        }
+        return value.to_string()
+    }
+
+
+
+    /// "Prettyfies" an `Unary` expression
+    /// - Parameter unary: The expression to transform
+    /// - Returns: The "prettyfied" expression
+    public func visit(_ unary: Unary) -> String
+    {
+        convert_to_RPN(name: unary.op.lexeme, expressions: unary.right)
+    }
+
+
+
+    /// Prints expressions wrapping grouping and nesting in parenthesis
+    /// - Parameters:
+    ///   - name:
+    ///   - expressions:
+    /// - Returns: The wrapped expression(s)
+    private func convert_to_RPN(name: String, expressions: Expression...) -> String
+    {
+        var output = ""
+        for expression in expressions
+        {
+            output += expression.accept(visitor: self) + " "
+        }
+
+        return output + name + " "
+    }
+}
--- Tests/SLoxTests/ASTPrinterTests.swift
+++ Tests/SLoxTests/ASTPrinterTests.swift
@@ -6,6 +6,7 @@ final class ASTPrinterTests: XCTestCase
     // Tests the example at the end of chapter 5.4
     func testBookExample()
     {
+        // -123 * (45.67)
         let expression = Binary(
             left: Unary(
                 op: Token(type: .MINUS, lexeme: "-", literal: nil, line: 1),
@@ -16,5 +17,27 @@ final class ASTPrinterTests: XCTestCase
         let result = ASTPrinter().print(expression: expression)
 
         XCTAssertEqual(result, "(* (- 123.0) (group 45.67))")
+    }
+
+
+
+    // Reverse Polish Notation (RPN). Exercise 5.3
+    func testRPN()
+    {
+        // (1 + 2) * (4 - 3)
+        let expression = Binary(
+            left: Binary(
+                left: LiteralExp(value: .number(1)),
+                op: Token(type: .PLUS, lexeme: "+", literal: nil, line: 1),
+                right: LiteralExp(value: .number(2))),
+            op: Token(type: .STAR, lexeme: "*", literal: nil, line: 1),
+            right: Binary(
+                left: LiteralExp(value: .number(4)),
+                op: Token(type: .MINUS, lexeme: "-", literal: nil, line: 1),
+                right: LiteralExp(value: .number(3))))
+
+        let result = ASTPrinterReversePolishNotation().print(expression: expression)
+
+        XCTAssertEqual(result, "1.0 2.0 +  4.0 3.0 -  * ")
     }
 }
